478ab38ce533c58694cee4ee0844ad8e7e78792f
carlos garcia <ambro_93garcas@hotmail.com>
2023-02-20 16:14:01 -0600

    urls name, view borrar

--- basico/favoritos/urls.py
+++ basico/favoritos/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import *
 
+app_name = 'favs'
 
 urlpatterns = [
     path('', index, name="home"),
     path('crear', crear_favoritos, name="crear"),
     path('lista', lista_favoritos, name="lista"),
+    path('borrar/<int:pk>', borrar_favoritos, name="borrar"),
 ]
--- basico/favoritos/views.py
+++ basico/favoritos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Favoritos
 from .forms import FavoritoModelForm
+from django.urls import reverse
 
 # Create your views here.
 
@@ -15,10 +16,10 @@ def crear_favoritos(request):
 
     if request.method == 'POST':
         form = FavoritoModelForm(request.POST)
-        
+
         if form.is_valid():
             form.save()
-            return redirect('crear')
+            return redirect('favs:lista')
         else:
             print(form.errors)
 
@@ -30,11 +31,16 @@ def crear_favoritos(request):
 
 
 def lista_favoritos(request):
-    
+
     favoritos_lista = Favoritos.objects.all()
-    
+
     context = {
         'favoritos_lista': favoritos_lista
     }
-    
+
     return render(request, 'favoritos/lista.html', context)
+
+
+def borrar_favoritos(request, pk):
+    Favoritos.objects.get(pk=pk).delete()
+    return redirect('favs:lista')
