424df942a916c5e3c59b287943ea8a3618fe8738
carlos garcia <ambro_93garcas@hotmail.com>
2023-03-09 18:06:49 -0600

    version final xd

--- basico/favoritos/urls.py
+++ basico/favoritos/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('crear', crear_favoritos, name="crear"),
     path('lista', lista_favoritos, name="lista"),
     path('borrar/<int:pk>', borrar_favoritos, name="borrar"),
+    path('detalle/<int:pk>', detalle_favoritos, name="detalle"),
+    path('actualizar/<int:pk>', actualizar_favoritos, name="actualizar"),
 ]
--- basico/favoritos/views.py
+++ basico/favoritos/views.py
@@ -44,3 +44,35 @@ def lista_favoritos(request):
 def borrar_favoritos(request, pk):
     Favoritos.objects.get(pk=pk).delete()
     return redirect('favs:lista')
+
+
+def detalle_favoritos(request, pk):
+    fav = Favoritos.objects.get(pk=pk)
+
+    context = {
+        'fav': fav
+    }
+
+    return render(request, 'favoritos/detalle.html', context)
+
+
+def actualizar_favoritos(request, pk):
+    
+    fav = Favoritos.objects.get(pk=pk)
+
+    form = FavoritoModelForm(instance=fav)
+
+    if request.method == 'POST':
+        form = FavoritoModelForm(request.POST, instance=fav)
+
+        if form.is_valid():
+            form.save()
+            return redirect('favs:lista')
+        else:
+            print(form.errors)
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'favoritos/crear.html', context)
